ddfae27f11b72cb09e698d328341a40726b4a337
Vasilis <b_gvas@yahoo.gr>
2024-09-12 23:54:29 +0300

    Table Creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {DataTableComponent} from "./modules/data-table/data-table.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: "full", redirectTo: '/main'},
+  {path: 'main', component: DataTableComponent}
+];
---
+++ src/app/modules/data-table/data-table.component.ts
@@ -0,0 +1,81 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatNoDataRow, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
+import {MatSort, MatSortHeader} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {CdkTableDataSourceInput} from "@angular/cdk/table";
+
+@Component({
+  selector: 'app-data-table',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatInput,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatSort,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatNoDataRow,
+    MatPaginator,
+    MatLabel,
+    MatSortHeader
+  ],
+  templateUrl: './data-table.component.html',
+  styleUrl: './data-table.component.scss'
+})
+export class DataTableComponent implements AfterViewInit{
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = [
+    "id",
+    "name",
+    "symbol",
+    "current_price",
+    "market_cap",
+    "total_volume",
+    "high_24h",
+    "low_24h",
+    "price_change_percentage_24h",
+    "circulating_supply"
+];
+
+  constructor() {
+    this.dataSource = new MatTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
