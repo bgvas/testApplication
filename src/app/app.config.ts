56a4e6092f64c8432f25bbdab8328e4b23b56512
Vasilis <b_gvas@yahoo.gr>
2024-09-13 01:03:03 +0300

    Creation of Service and interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,9 @@ import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from
 import {provideRouter, RouterModule} from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {HttpClient, provideHttpClient, withInterceptors} from "@angular/common/http";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {dataInterceptor} from "./interceptors/data.interceptor";
-import {errorInterceptor} from "./interceptors/error.interceptor";
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,16 +14,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     importProvidersFrom([
       RouterModule.forRoot(routes, {useHash: true}),
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      }),
     ]),
-    provideHttpClient(withInterceptors([
-      dataInterceptor, errorInterceptor
-    ])),
+    provideHttpClient(
+      withInterceptors([
+          dataInterceptor
+      ])
+    ),
   ]
 };
