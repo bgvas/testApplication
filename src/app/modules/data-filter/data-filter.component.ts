4f2f267b718a7535733f7aa55af8643c9b85aca1
Vasilis <b_gvas@yahoo.gr>
2024-09-14 10:24:20 +0300

    Filter buttons alignment

--- src/app/modules/data-filter/data-filter.component.ts
+++ src/app/modules/data-filter/data-filter.component.ts
@@ -1,15 +1,17 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-data-filter',
   standalone: true,
-    imports: [
-        MatFormField,
-        MatInput,
-        MatLabel
-    ],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatButton
+  ],
   templateUrl: './data-filter.component.html',
   styleUrl: './data-filter.component.scss',
   encapsulation: ViewEncapsulation.None
